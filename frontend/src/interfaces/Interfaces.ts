fc3fa5b760fdb6c875d8d11a0c969e409509161d
jiheunkim <ziheun54@gmail.com>
2024-06-18 03:30:26 +0900

    Feat: 타입 interface 정의 #6

--- frontend/src/interfaces/Interfaces.ts
+++ frontend/src/interfaces/Interfaces.ts
@@ -7,8 +7,15 @@ export interface LoginResponse {
     token: string;
 }
 
-export interface EmailResponse {
+export interface CheckBooleanResponse {
     isExist: boolean;
+}
+
+export interface MyUser {
+    email: string;
+    nickname: string;
+    profile: string;
+    baby: Baby[];
 }
 
 export interface User {
@@ -17,3 +24,58 @@ export interface User {
 }
 
 export interface Users extends Array<User> {}
+
+export interface Baby {
+    name: string;
+    profile: string;
+}
+
+export interface MakeVocaCard {
+    category: string;
+    description: string;
+    age: number;
+    language: number;
+}
+
+export interface VocaTheme {
+    category: string;
+    description: string;
+}
+
+export interface VocaThemes extends Array<VocaTheme> {}
+
+export interface AllVocaTheme {
+    category: string;
+    description: string;
+    nickname: string;
+}
+
+export interface AllVocaThemeCard {
+    category: string;
+    description: string;
+    nickname: string;
+    cards: VocaCard[];
+}
+
+export interface VocaThemeCard {
+    category: string;
+    description: string;
+    cards: VocaCard[];
+}
+
+export interface VocaCard {
+    src: string;
+    kor: string;
+    eng: string;
+    jap: string;
+    chi: string;
+    example: string;
+}
+
+export interface VocaPrint {
+    category: string;
+    description: string;
+    nickname: string;
+    type: number;
+    template: number;
+}
