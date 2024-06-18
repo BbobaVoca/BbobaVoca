342a1cbc0f5548df0dc50a0bf6c08851cfa6fe7e
jiheunkim <ziheun54@gmail.com>
2024-06-18 23:28:19 +0900

    Fix: 시작 배치 및 디자인 수정

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -10,20 +10,24 @@ function HeroSection() {
 
   return (
     <div className='text-center'>
-        <div className='flex w-screen justify-center self-stretch bg-main'>
-            <div className='flex flex-1 flex-col md:flex-row box-border max-w-screen-xl items-center justify-start px-5 md:px-20 xl:px-10 pt-20 pb-20'>
-                <div className='flex-1 flex-grow-4 self-start max-w-none prose-lg mx-4 '>
-                    <div className='flex justify-center my-3 mb-0'>
-                        <div className="flex flex-1 flex-col pt-20 pb-20">
-                            <img
-                                className="flex mx-auto w-96"
-                                src={`/img/herosection-character.png`}
-                            />
-                            <p className="font-PretendardVariable text-3xl text-white">AI 기반 단어 생성 서비스</p>
-                            <br></br>
-                            <p className="font-ownglyph text-7xl text-white">뽑아보카</p>
-                        </div>
-                    </div>
+        <div className='flex w-screen min-h-screen justify-center self-stretch bg-main'>
+            <div className='flex flex-1 flex-row box-border max-w-screen-xl items-center justify-center'>
+                <div className="flex flex-1 justify-center items-center pl-20">
+                    <img
+                        className="w-4/5"
+                        src={`/img/herosection-character.png`}
+                    />
+                </div>
+                <div className="flex flex-1 flex-col justify-center items-start text-left">
+                    <p className="font-PretendardVariable text-3xl text-white pb-3">AI 기반 단어 생성 서비스</p>
+                    <p className="font-ownglyph text-7xl text-white pb-8">뽑아보카</p>
+                    <button
+                        type="submit"
+                        className="rounded-lg bg-white py-4 px-14 font-PretendardVariable text-2xl font-semibold leading-tight text-black shadow-md transition duration-200 ease-in-out cursor-pointer"
+                        onClick={moveLoginBtn}
+                    >
+                        메인페이지 바로가기
+                    </button>
                 </div>
             </div>
         </div>
@@ -33,7 +37,13 @@ function HeroSection() {
             src={`/img/Herosection.png`}
         />
         <br></br><br></br><br></br><br></br>
-        <button type="submit" className="relative mx-auto justify-center rounded-lg bg-main py-4 px-14 font-PretendardVariable text-2xl font-semibold leading-tight text-white shadow-md transition duration-200 ease-in-out cursor-pointer" onClick={moveLoginBtn}>메인페이지 바로가기</button>
+        <button
+            type="submit"
+            className="relative mx-auto justify-center rounded-lg bg-main py-4 px-14 font-PretendardVariable text-2xl font-semibold leading-tight text-white shadow-md transition duration-200 ease-in-out cursor-pointer"
+            onClick={moveLoginBtn}
+        >
+            메인페이지 바로가기
+        </button>
         <br></br><br></br><br></br><br></br><br></br>
     </div>
   );
