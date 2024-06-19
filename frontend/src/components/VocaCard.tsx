503035338c3dfd5f351b374029e1a1d6f42bdfb5
jiheunkim <ziheun54@gmail.com>
2024-06-20 02:33:35 +0900

    Feat: 한국어만 우선 tts 기능 적용

--- frontend/src/components/VocaCard.tsx
+++ frontend/src/components/VocaCard.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { getSpeech } from './getSpeech';
+import { MdOutlineSpeaker, MdOutlineSpeakerNotes, MdSpeaker } from 'react-icons/md';
 
 function VocaCard(props: {
     bgColor: string;
@@ -7,22 +9,31 @@ function VocaCard(props: {
     src: string;
     example: string;
 }) {
+    const handleClick = () => {
+        getSpeech(props.kor);
+    };
+
     return (
         <div className={`flex flex-1 ${props.bgColor} rounded-3xl mx-2 mb-2 shadow-inner outline outline-1 outline-neutral-200`}>
             <figure className='relative w-full h-full flex flex-col cursor-pointer'>
-                <div className='flex justify-between py-8 px-20'>
+                <div className='flex justify-between py-8 px-10'>
                     <div className='mt-10'>
                         <img
-                        className="w-96 pl-12 pb-5"
-                        src={props.src}
-                        alt="vocaImage"
+                            className="w-96 pl-12 pb-5"
+                            src={props.src}
+                            alt="vocaImage"
                         />
                     </div>
-                    <div className={`relative rounded-xl object-cover w-full ${props.bgColor}}`}>
+                    <div className={`relative rounded-xl object-cover w-full ${props.bgColor}`}>
                         <div className="relative flex justify-center items-center font-ownglyph text-6xl py-10">
-                            <p>
+                            <p className='flex'>
                                 {props.kor}
-                                <span className='font-Maplestory text-3xl ml-4'>{props.other}</span>
+                                <span className='font-Maplestory text-3xl ml-4 mt-3'>{props.other}</span>
+                                <img
+                                    className='w-8 h-8 ml-4 mt-4'
+                                    src="/img/speaker.png"
+                                    onClick={handleClick}
+                                />
                             </p>
                         </div>
                         <p className='relative flex justify-center pb-10 font-ownglyph text-4xl'>
