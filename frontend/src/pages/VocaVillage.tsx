eca63d88bc9a886c295012d3c76f3994731aee91
jiheunkim <ziheun54@gmail.com>
2024-06-22 21:55:23 +0900

    Fix: 높이 min-h-screen으로 변경

--- frontend/src/pages/VocaVillage.tsx
+++ frontend/src/pages/VocaVillage.tsx
@@ -51,7 +51,7 @@ const VocaVillage = () => {
             <HeroSection />
             </>
         ) : (
-            <div className='flex w-screen h-full justify-center self-stretch text-gray-700 bg-light-green'>
+            <div className='flex w-screen min-h-screen justify-center self-stretch text-gray-700 bg-light-green'>
                 <div className='flex flex-1 flex-col md:flex-row box-border max-w-screen-xl items-center justify-start px-5 md:px-20 xl:px-10 pt-20 pb-20'>
                     <div className='flex-1 flex-grow-4 self-start max-w-none prose-lg mx-4 text-gray-700'>
                         <div id="content-container" className='mx-auto md:w-[80%]'>
