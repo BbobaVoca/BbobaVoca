e7736c3f5c9ea602d911b43938d4820f0058aa86
jiheunkim <ziheun54@gmail.com>
2024-06-23 16:02:56 +0900

    Refactoring: css 수정 및 코드 약간 정리

--- frontend/src/pages/MyPage.tsx
+++ frontend/src/pages/MyPage.tsx
@@ -1,14 +1,13 @@
 import { useNavigate } from "react-router-dom";
 import { useRecoilState } from 'recoil';
 import { savedUserState } from '../atom';
-import { FaEdit } from 'react-icons/fa'; // Importing the edit icon from react-icons
+import { FaEdit, FaRegEdit } from 'react-icons/fa';
 import { useState, useEffect } from "react";
-import { makeTimeline, getTimeline, updateProfile } from "../api/bbobavoca/bbobavocaAxios"; // 백엔드 API 호출 함수 import
-import { profile } from "console";
-import handleLogoutButton from "../components/MyInfoCard"
+import { makeTimeline, getTimeline, updateProfile } from "../api/bbobavoca/bbobavocaAxios";
 import { removeUserFromLocalStorage, saveUserToLocalStorage } from '../utils/localStorage';
 import { user } from "../api/user/userAxios";
 import { TimelineMessage } from "../interfaces/Interfaces";
+import { MdEdit } from "react-icons/md";
 
 const MyPage = () => {
     const navigate = useNavigate();
@@ -17,6 +16,7 @@ const MyPage = () => {
     const [userInfo, setUserInfo] = useRecoilState(savedUserState);
     const [showPopup, setShowPopup] = useState(false);
     const [showPopup2, setShowPopup2] = useState(false);
+    const [showMessage, setShowMessage] = useState(true);
     const [message, setMessage] = useState(""); // State for the message input
     const [profile, setProfile] = useState<File | null>(null); // State for the profile file input
     const [timeline, setTimeline] = useState<TimelineMessage>(); // 타임라인 데이터 상태
@@ -28,7 +28,7 @@ const MyPage = () => {
             const response = await getTimeline(token);
             if (response && response.data) {
                 setTimeline(response.data);
-                console.log(timeline);
+                if (response.data.msg !== "") setShowMessage(false);
             }
         }
     };
@@ -50,12 +50,13 @@ const MyPage = () => {
         localStorage.removeItem('token');
         removeUserFromLocalStorage();
         setUserInfo(null);
-        //props.onLogout();
+        navigate('/');
     };
 
 
     const handlePopUpButtonClick = () => {
         setShowPopup(true);
+        setShowMessage(false);
     };
 
     const handlePopUpButtonClick2 = () => {
@@ -67,14 +68,18 @@ const MyPage = () => {
         setShowPopup2(true);
     };
 
-    // Function to handle closing the popup
     const handleClosePopup = () => {
         setShowPopup(false);
     };
+
     const handleClosePopup2 = () => {
         setShowPopup2(false);
     };
-    // Function to handle submitting the message
+
+    const handleCloseMessage = () => {
+        setShowMessage(false);
+    }
+
     const handleSubmitMessage = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (token) {
@@ -104,9 +109,8 @@ const MyPage = () => {
     const handleImageUpload = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (token && profile) {
-            console.log("백엔드 통신 시도중2")
             // setIsLoading(true);
-            const babyName = userInfo?.babies?.name; // 아기 이름 추출
+            const babyName = userInfo?.babies?.name;
             const formData = new FormData();
             if (babyName != null) {
                 formData.append('name', babyName)
@@ -178,151 +182,77 @@ const MyPage = () => {
                     </div>
                 </div>
                 {/* Main Content */}
-                <div className='flex w-screen min-h-screen justify-center self-stretch bg-light-green'>
-                    <div className='flex-1 flex-grow-4s self-start max-w-none prose-lg mx-4 text-gray-700'>
-                        <div className="flex flex-col h-screen items-center rounded-lg p-6">
-
-
-                            {/* {timeline && timeline.timelineMessages.length > 0 ? (
-                                <div className='flex flex-col items-center mt-4'>
-                                </div>
-                            ) : ( */}
-                            <div className="flex text-center mt-5 px-20 py-2 bg-main text-black rounded-xl flex md:w-3/4 items-center justify-between">
-                                <div className="flex-1 text-center">
+                <div className='flex flex-1 flex-col md:flex-row box-border max-w-screen-xl justify-center items-center ml-20 pt-10 pb-20'>
+                    <div className='flex-1 self-center max-w-none prose-lg text-gray-700'>
+                        <div className="flex flex-col h-full rounded-lg p-6">
+                            {showMessage && (
+                                <div className="flex px-5 py-4 bg-green-400 text-white rounded-xl flex md:w-3/4 items-center justify-between">
                                     <div>
-                                        아직 메세지를 작성하지 않았어요. 내 아이를 위한 메세지를 남겨볼까요?
+                                        <div className="text-start">
+                                            아직 메세지를 작성하지 않았어요. 내 아이를 위한 메세지를 남겨볼까요?
+                                        </div>
+                                        <button
+                                            type="submit"
+                                            onClick={handlePopUpButtonClick}
+                                            className="mt-5 px-5 py-2 text-sm bg-gray-50 text-black rounded border border-gray-300">
+                                            작성하기
+                                        </button>
                                     </div>
-                                    <button
-                                        type="submit"
-                                        onClick={handlePopUpButtonClick}
-                                        className="ml-100 mt-5 px-4 py-2 bg-gray-50 text-black rounded border border-gray-300">
-                                        작성하기
-                                    </button>
                                 </div>
-                            </div>
-                            {/* )
-                            } */}
-
-
-
+                            )}
 
                             {/* Changed section starts here */}
-                            <div className="flex items-center mt-10 w-full">
-                                <h2 className="text-3xl font-semibold">단어 성장 일지</h2>
-                                <button className="ml-2 px-2 py-1 bg-gray-50 text-gray-600 rounded-full border border-gray-300"
-                                    onClick={handlePopUpButtonClick}>
-                                    <FaEdit />
-                                </button>
+                            <div className="flex mt-5 py-3 text-black flex md:w-3/4 border-b border-gray-300 mb-2">
+                                <h2 className="text-xl font-semibold ml-2">단어 성장 일지</h2>
+                                    <button className="px-2 py-1 text-gray-600"
+                                        onClick={handlePopUpButtonClick}>
+                                        <MdEdit />
+                                    </button>
                             </div>
-                            {/* Changed section ends here */}
-                            <div className="mt-2 border-b border-gray-300 w-full"></div>
-                            <div className="flex justify-center relative">
-                                <img
-                                    className="w-full mt-10 rounded-lg"
-                                    src={`/img/mypagePic.png`}
-                                    alt="뽑아보카 이미지"
-                                />
-                                <div className='absolute top-5 left-0 right-0 bottom-0 flex flex-col items-center p-4 overflow-auto'>
-                                {/* <div className="bg-white p-4 rounded-full shadow-lg mt-8 mb-4 text-center">
-    <h1 className='text-2xl font-bold text-black'>
-        {userInfo?.babies?.name || '아기'}의 순간들
-    </h1>
-</div> */}
-<span className="inline-flex items-center rounded-md bg-gray-50 px-2 py-1 text-3xl font-uhbeezziba text-gray-600 ring-1 ring-inset ring-gray-500/10 mt-10">{userInfo?.babies?.name || '아기'}의 순간들</span>
-
-                                    {/* 타임라인 메시지 렌더링 */}
-                                    {/* {timeline && timeline.timelineMessages.length > 0 ? ( */}
-
-
-                                    {/* 
-                                    {timeline != null ? ( 
-                                        <div className='flex flex-col items-center mt-4'>
-                                            {timeline.timelineMessages.map((message, index) => (
-                                                <div key={index} className='max-w-lg bg-white bg-opacity-80 rounded-lg p-4 mb-4'>
-                                                    <div className='flex items-center mb-2'>
-                                                        <img src={message.babies.profile} alt={message.babies.name} className='w-12 h-12 rounded-full object-cover mr-2' />
-                                                        <div>
-                                                            <h2 className='text-lg font-semibold'>{message.babies.name}</h2>
-                                                            <p className='text-gray-600'>{message.msg}</p>
+                            <div className="flex py-2 text-black flex md:w-3/4">
+                                <div className="relative w-full">
+                                    <img
+                                        className="w-full rounded-lg"
+                                        src={`/img/mypagePic.png`}
+                                        alt="myPage"
+                                    />
+                                    <div className='absolute inset-0 flex flex-col items-center justify-center p-4 overflow-auto'>
+                                        <span className="inline-flex mb-7 items-center rounded-3xl bg-gray-50 px-7 py-3 text-3xl font-ownglyph text-gray-600 ring-1 ring-inset ring-gray-500/10 mt-10 lg:text-4xl lg:px-10 lg:py-5">
+                                            {userInfo?.babies?.name || '내 아이'}의 순간들
+                                        </span>
+                                        {timeline != null ? (
+                                            <div className='flex flex-col items-center mt-4'>
+                                                <img
+                                                    className="w-40 h-40 object-cover rounded-full lg:w-60 lg:h-60"
+                                                    alt="profile"
+                                                    src={timeline.babies.profile}
+                                                />
+                                                <span className="font-uhbeezziba items-center text-2xl mt-10 mb-10 lg:text-4xl">{timeline.msg}</span>
+                                                <div className="mt-10 items-center">
+                                                    {timeline.vocas.map((vocaData, vocaIndex) => (
+                                                        <div key={vocaIndex} className='flex items-center mb-2'>
+                                                            <span className='font-uhbeezziba font-semibold mr-2 text-lg lg:text-2xl'>{vocaData.timestamp}</span>
+                                                            <span className="font-uhbeezziba text-lg lg:text-2xl">{vocaData.voca.join(', ')}</span>
                                                         </div>
-                                                    </div>
-                                                    <div className='mt-2'>
-                                                        {/* VocaData를 반복적으로 렌더링 */}
-                                    {/* {Object.keys(message.vocas).map((key: string) => (
-                                                            <div key={key} className='flex items-center mb-2'>
-                                                                <span className='font-semibold mr-2'>{message.vocas[key as any].timestamp}</span>
-                                                                <ul className='list-disc ml-4'>
-                                                                    {message.vocas[key as any].voca.map((word: string, i: number) => (
-                                                                        <li key={`${key}-${i}`}>{word}</li>
-                                                                    ))}
-                                                                </ul>
-                                                            </div>
-                                                        ))}
-
-                                                    </div>
+                                                    ))}
                                                 </div>
-                                            ))}
-                                        </div>
-                                    ) : (
-                                        <div className='flex flex-col items-center justify-center bg-white bg-opacity-80 rounded-lg p-6 mt-40'>
-                                            <p className='text-xl font-semibold text-gray-800 mb-2'>아직 생성된 단어가 없습니다!</p>
-                                            <p className='text-lg text-gray-600'>단어를 생성해주세요</p>
-                                        </div>
-                                    )} */}
-
-                                    {timeline != null ? (
-                                        <div className='flex flex-col items-center mt-4'>
-
-                                            <img
-                                                className="w-40 h-40 object-cover rounded-full"
-                                                alt="profile"
-                                                src={timeline.babies.profile}
-                                            />
-                                            <span className="font-uhbeezziba items-center text-2xl mt-10">{timeline.msg}</span>
-
-
-                                            <div className="mt-10 items-center">
-                                                {/* <span className="font-uhbeezziba items-center text-2xl">{timeline.msg}</span> */}
-
-                                                {timeline.vocas.map((vocaData, vocaIndex) => (
-                                                    <div key={vocaIndex} className='flex items-center mb-2'>
-                                                        <span className='font-uhbeezziba font-semibold mr-2'>{vocaData.timestamp}</span>
-                                                        <span className="font-uhbeezziba">{vocaData.voca.join(', ')}</span>
-                                                        <ul className='list-disc ml-4'>
-                                                            {/* {vocaData.voca.map((word, wordIndex) => (
-                                    <li key={`${vocaIndex}-${wordIndex}`}>{word}</li>
-                                ))} */}
-                                                        </ul>
-                                                    </div>
-                                                ))}
                                             </div>
-
-                                        </div>
-                                    ) : (
-                                        <div className='flex flex-col items-center justify-center bg-white bg-opacity-80 rounded-lg p-6 mt-40'>
-                                            <p className='text-xl font-semibold text-gray-800 mb-2'>아직 생성된 단어가 없습니다!</p>
-                                            <p className='text-lg text-gray-600'>단어를 생성해주세요</p>
-                                        </div>
-                                    )}
-
-
-
-
-
-
-
-
+                                        ) : (
+                                            <div className='flex flex-col items-center justify-center bg-white bg-opacity-80 rounded-lg p-6 mt-40'>
+                                                <p className='text-xl font-semibold text-gray-800 mb-2 lg:text-3xl'>아직 생성된 단어가 없습니다!</p>
+                                                <p className='text-lg text-gray-600 lg:text-2xl'>단어를 생성해주세요</p>
+                                            </div>
+                                        )}
+                                    </div>
                                 </div>
                             </div>
-
-
-
+                            {/* Changed section ends here */}
                         </div>
                     </div>
                 </div>
             </div>
-            {/* Message popup */}
 
+            {/* Message popup */}
             {showPopup2 && (
                 <div className="fixed top-0 left-0 w-full h-full bg-gray-500 bg-opacity-60 flex items-center justify-center">
                     <div className="bg-white rounded-lg border-2 border-gray-300 p-6 w-96">
@@ -366,7 +296,6 @@ const MyPage = () => {
                         />
                         <div className="flex justify-end">
                             <form onSubmit={handleSubmitMessage}>
-                                <input type="text" value={message} onChange={handleInputChange} />
                                 <div className="flex justify-end mt-4">
                                     <button onClick={handleClosePopup} className="px-4 py-2 bg-gray-200 text-gray-700 rounded-md mr-2">취소</button>
                                     <button type="submit" className="px-4 py-2 bg-green-500 text-white rounded-md">저장</button>
@@ -376,7 +305,6 @@ const MyPage = () => {
                     </div>
                 </div>
             )}
-            {/* End of Message popup */}
         </div>
     );
 };
