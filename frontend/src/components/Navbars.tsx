9b7dff3a98f6ee4023fec022cca9a4495796f9e7
jiheunkim <ziheun54@gmail.com>
2024-06-29 01:48:35 +0900

    Fix: 로그인/회원가입 페이지에서 헤더 컴포넌트에 로고만 보이게

--- frontend/src/components/Navbars.tsx
+++ frontend/src/components/Navbars.tsx
@@ -74,44 +74,48 @@ function Navbars() {
           뽑아보카
         </span>
       </Navbar.Brand>
-      <div className="flex items-center mr-auto gap-x-14 ml-28 list-none">
-        <Navbar.Link theme={theme} href="/" className={getButtonStyle("/")}>
-          단어보기
-        </Navbar.Link>
-        <Navbar.Link theme={theme} href="/voca-village" className={getButtonStyle("/voca-village")}>
-          단어마을
-        </Navbar.Link>
-      </div>
-      <div className="flex items-center ml-auto gap-x-4 mr-8 list-none">
-        {loggedIn ? (
-          <div className="flex items-center list-none">
-            <div className="flex w-full">
-              <img
-                  className="w-6 mr-1"
-                  alt='credit'
-                  src="img/credit.png"
-              />
-              <p>{userInfo?.credit}</p>
-            </div>
-            <div className="rounded-full overflow-hidden w-24">
-              <img
-                  className="h-full w-full object-cover rounded-full"
-                  alt='profile'
-                  src={userInfo?.babies.profile}
-              />
-            </div>
-            <div className='flex w-full ml-3 tracking-wide' onClick={handleMyInfo}>
-              <p className="font-semibold text-black">{userInfo?.nickname}
-                <span className='font-medium'>님</span>
-              </p>
-            </div>   
+      {location.pathname !== '/login' && location.pathname !== '/register' && (
+        <>
+          <div className="flex items-center mr-auto gap-x-14 ml-28 list-none">
+            <Navbar.Link theme={theme} href="/" className={getButtonStyle("/")}>
+              단어보기
+            </Navbar.Link>
+            <Navbar.Link theme={theme} href="/voca-village" className={getButtonStyle("/voca-village")}>
+              단어마을
+            </Navbar.Link>
           </div>
-        ) : (
-          <Button color="light" className={`font-semibold hover:bg-green-600 rounded-3xl px-5 mr-2 bg-main text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent ${activeLink === 'login' ? 'active' : ''}`} onClick={handleLoginButtonClick}>
-            로그인
-          </Button>
-        )}
-      </div>
+          <div className="flex items-center ml-auto gap-x-4 mr-8 list-none">
+          {loggedIn ? (
+            <div className="flex items-center list-none">
+              <div className="flex w-full">
+                <img
+                    className="w-6 mr-1"
+                    alt='credit'
+                    src="img/credit.png"
+                />
+                <p>{userInfo?.credit}</p>
+              </div>
+              <div className="rounded-full overflow-hidden w-24">
+                <img
+                    className="h-full w-full object-cover rounded-full"
+                    alt='profile'
+                    src={userInfo?.babies.profile}
+                />
+              </div>
+              <div className='flex w-full ml-3 tracking-wide' onClick={handleMyInfo}>
+                <p className="font-semibold text-black">{userInfo?.nickname}
+                  <span className='font-medium'>님</span>
+                </p>
+              </div>   
+            </div>
+          ) : (
+            <Button color="light" className={`font-semibold hover:bg-green-600 rounded-3xl px-5 mr-2 bg-main text-white dark:hover:bg-gray-700 md:dark:hover:bg-transparent ${activeLink === 'login' ? 'active' : ''}`} onClick={handleLoginButtonClick}>
+              로그인
+            </Button>
+          )}
+          </div>
+        </>
+      )}
     </Navbar>
     {activeMyInfo && (
       <>
